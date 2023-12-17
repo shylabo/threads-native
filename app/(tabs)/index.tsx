74858b045e89c200d842b27cb0b1b568d459d49b
shylabo <shuya.sohot@gmail.com>
2023-12-16 21:51:45 -0800

    implement: base threads

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,43 @@
-import { StyleSheet } from 'react-native';
+import * as React from 'react'
+import Lottie from 'lottie-react-native'
+import { RefreshControl } from 'react-native-gesture-handler'
+import { Platform, SafeAreaView, ScrollView } from 'react-native'
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { ThreadsContext } from '../../context/thread-context'
+import ThreadsItem from '../../components/ThreadsItem'
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
-}
+  const animationRef = React.useRef<Lottie>(null)
+  const threads = React.useContext(ThreadsContext)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {
+              animationRef.current?.play()
+            }}
+            tintColor={'transparent'}
+          />
+        }
+      >
+        <Lottie
+          ref={animationRef}
+          source={require('../../lottie-animations/threads.json')}
+          loop={false}
+          autoPlay
+          style={{ width: 90, height: 90, alignSelf: 'center' }}
+        />
+        {threads.map((thread) => (
+          <ThreadsItem key={thread.id} {...thread} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
